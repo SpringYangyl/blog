cc737588cab512b272607ece096c6ffeb4a22aaf
SpringYangyl <95664475+SpringYangyl@users.noreply.github.com>
2022-06-08 12:11:47 +0800

    Update UpLoadServiceImpl.java

--- framework/src/main/java/com/yyl/service/impl/UpLoadServiceImpl.java
+++ framework/src/main/java/com/yyl/service/impl/UpLoadServiceImpl.java
@@ -21,15 +21,7 @@ public class UpLoadServiceImpl implements UpLoadService {
         return ResponseResult.okResult(url);
     }
     private String uploadOSS(MultipartFile img){
-        String endpoint = "oss-cn-beijing.aliyuncs.com";
-        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5t6QR8FF4u2FjYuA9ghf";
-        String accessKeySecret = "uEydst0664UWivI0WnozfSSR5nhWPG";
-        // 填写Bucket名称，例如examplebucket。
-        String bucketName = "edu-138";
-        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
-        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
-        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件。
+    
 
         String objectName = UUID.randomUUID() +".jpg";
         // 创建OSSClient实例。
